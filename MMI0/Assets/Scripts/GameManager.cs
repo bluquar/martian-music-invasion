66ae2b2df332ee9e72652488a9c8881eb3d845b2
Jennifer Han <jhan2@andrew.cmu.edu>
2015-11-22 05:26:39 -0500

    all lock tiles added and working

--- MMI0/Assets/Scripts/GameManager.cs
+++ MMI0/Assets/Scripts/GameManager.cs
@@ -4,8 +4,8 @@ using System.Collections;
 public class GameManager : MonoBehaviour {
 
 	static public int currentLevel = 1;
-	static public bool integratedVersion = true;
-	static public int numOfLevels = 3;
+	static public bool integratedVersion = false;
+	static public int numOfLevels = 18;
 
 	void Awake() {
 		// Do not destory this GameManager instance when changing scenes in order to store data
