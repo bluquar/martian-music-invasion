058a69f9fa5e0c6d325e5d278daef564fa8d997b
Chris Barker <bluquar@gmail.com>
2015-11-30 15:44:43 -0500

    sounds in cutscenes!

---
+++ MMI0/Assets/CutsceneAudio.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CutsceneAudio : MonoBehaviour {
+
+	public AudioClip orchestralMusic;
+	public AudioClip alienMusic;
+
+	public AudioSource audioSource;
+
+	public static CutsceneAudio singleton;
+
+	// Use this for initialization
+	void Start () {
+		CutsceneAudio.singleton = this;
+		DontDestroyOnLoad (this);
+		this.audioSource = this.gameObject.GetComponent<AudioSource> ();
+		this.audioSource.clip = orchestralMusic;
+		this.audioSource.Play ();
+	}
+
+	public static void ChangeScene (string sceneName) {
+		CutsceneAudio ca = CutsceneAudio.singleton;
+		if (sceneName == "IntroCutscene1") {
+			ca.audioSource.clip = ca.alienMusic;
+			ca.audioSource.Play ();
+		} else if (sceneName == "LevelSelection") {
+			Destroy(ca.gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
